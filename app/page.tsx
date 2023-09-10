ef080ce0b106f3fca3f752ea8d45f41d744473c4
Kunal Keshan <kunalkeshan12@gmail.com>
2023-09-10 20:17:33 +0530

    chore: hero section for home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import Contact from '@/components/home/Contact';
+import FAQ from '@/components/home/FAQ';
+import Gallery from '@/components/home/Gallery';
+import Hero from '@/components/home/Hero';
+import Services from '@/components/home/Services';
+import Testimonials from '@/components/home/Testimonials';
+
 export default function Home() {
 	return (
-		<main>
-			<h1>Hello, world!</h1>
+		<main className=''>
+			<Hero />
+			<Services />
+			<Gallery />
+			<Testimonials />
+			<Contact />
+			<FAQ />
 		</main>
 	);
 }
