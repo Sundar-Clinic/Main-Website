858fc10f71db21d94bc406a411db52fd0a42c0a5
kunalkeshan <kunalkeshan12@gmail.com>
2023-09-11 20:12:50 +0530

    feat: added faqs section for landing

--- components/home/FAQ.tsx
+++ components/home/FAQ.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { FAQS } from '@/constants/clinic';
+import {
+	Accordion,
+	AccordionContent,
+	AccordionItem,
+	AccordionTrigger,
+} from '@/components/ui/accordion';
 
 const FAQ = () => {
-	return <div>FAQ</div>;
+	return (
+		<section className='max-w-3xl mx-auto w-full p-4 mt-8'>
+			<h3 className='text-xl font-heading text-center'>
+				Frequently Asked Questions
+			</h3>
+			<hr className='border-b-4 max-w-[10rem] border-b-primary-clinic rounded-lg mx-auto mt-2' />
+			<Accordion type='single' collapsible className='w-full mt-8'>
+				{FAQS.map((faq) => (
+					<AccordionItem
+						key={`faq-${faq.question}`}
+						value={faq.question}
+					>
+						<AccordionTrigger>{faq.question}</AccordionTrigger>
+						<AccordionContent>{faq.answer}</AccordionContent>
+					</AccordionItem>
+				))}
+			</Accordion>
+		</section>
+	);
 };
 
 export default FAQ;
--- constants/clinic.ts
+++ constants/clinic.ts
@@ -15,11 +15,13 @@ import {
 	BadgePlus,
 } from 'lucide-react';
 
-type SocialData = ReadonlyArray<{
-	name: string;
-	Icon: LucideIcon;
-	url: string;
-}>;
+type SocialData = ReadonlyArray<
+	Readonly<{
+		name: string;
+		url: string;
+		Icon: LucideIcon;
+	}>
+>;
 
 type ContactData = Readonly<{
 	phone: string;
@@ -28,11 +30,20 @@ type ContactData = Readonly<{
 	email: string;
 }>;
 
-export type ServiceData = ReadonlyArray<{
-	title: string;
-	description: string;
-	Icon: LucideIcon;
-}>;
+export type ServiceData = ReadonlyArray<
+	Readonly<{
+		title: string;
+		description: string;
+		Icon: LucideIcon;
+	}>
+>;
+
+type FAQData = ReadonlyArray<
+	Readonly<{
+		question: string;
+		answer: string;
+	}>
+>;
 
 export const CONTACTS: ContactData = {
 	phone: '+918939881708',
@@ -99,5 +110,40 @@ export const SERVICES: ServiceData = [
 		description:
 			'Explore the full scope of our offerings by visiting us. We are continuously expanding our services to better serve you.',
 		Icon: BadgePlus,
+	},
+];
+
+export const FAQS: FAQData = [
+	{
+		question: "What are your clinic's operating hours?",
+		answer: 'Sundar Clinic is open Monday through Sunday from 9:00 AM to 1:30 PM and 4:00 PM to 9:30 PM.',
+	},
+	{
+		question: 'How can I schedule an appointment?',
+		answer: `You can schedule an appointment by calling our clinic during business hours at ${CONTACTS.phone}.`,
+	},
+	{
+		question: 'What types of payment methods do you accept?',
+		answer: 'We accept various payment methods, including cash, credit/debit cards, and UPI.',
+	},
+	{
+		question: 'Are walk-in patients accepted?',
+		answer: 'While we encourage scheduling appointments to ensure prompt service, we do accept walk-in patients. Please be aware that scheduled appointments may take priority.',
+	},
+	{
+		question: 'What should I bring to my appointment?',
+		answer: 'Please bring your photo ID, insurance card, a list of current medications, and any relevant medical records or test results to your appointment.',
+	},
+	{
+		question: 'How can I request prescription refills?',
+		answer: 'You can request prescription refills by calling our clinic, and our staff will assist you with the process. Please allow sufficient time for us to process your request.',
+	},
+	{
+		question: 'Is parking available at the clinic?',
+		answer: 'Yes, we have ample parking available for our patients at our clinic location.',
+	},
+	{
+		question: 'What should I do in case of a medical emergency?',
+		answer: 'In the event of a medical emergency, please call 112 (national helpline) or 102 (ambulance) immediately or go to the nearest emergency room. For non-emergency medical concerns, contact our clinic during business hours.',
 	},
 ];
