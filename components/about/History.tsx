b49e2006e8fa9a9e296224e220d0b57ffeecc6c4
kunalkeshan <kunalkeshan12@gmail.com>
2023-09-16 09:10:46 +0530

    chore: history section content added

--- components/about/History.tsx
+++ components/about/History.tsx
@@ -3,21 +3,37 @@ import React from 'react';
 const History = () => {
 	return (
 		<section className='w-full mt-8 bg-slate-400/50' id='history'>
-			<div className='p-4 max-w-7xl mx-auto grid grid-cols-1 place-items-center h-full min-h-[60vh]'>
-				<div className='bg-white p-4 rounded-lg shadow-lg max-w-4xl flex flex-col gap-4'>
-					<p className='font-medium text-lg md:text-xl'>History</p>
-					<h1 className='font-heading font-medium text-2xl md:text-4xl max-w-3xl'>
-						Welcome to Sundar Clinic: Where Healthcare Meets
-						Exceptional Experience
-					</h1>
+			<div className='px-4 py-8 max-w-7xl mx-auto grid grid-cols-1 h-full min-h-screen gap-2'>
+				<p className='font-medium text-lg md:text-xl flex items-center justify-center'>
+					History
+				</p>
+				<div className='bg-white p-4 rounded-lg shadow-lg max-w-4xl flex flex-col gap-4 mx-auto'>
+					<h2 className='font-heading font-medium text-2xl md:text-4xl max-w-3xl'>
+						Our Journey: Transforming Healthcare since 2013
+					</h2>
 					<p className='text-lg md:text-xl text-justify max-w-5xl text-slate-500'>
-						At Sundar Clinic, we&apos;re not just about healthcare;
-						we&apos;re about crafting a better healthcare
-						experience. Since our establishment in 2013, we&apos;ve
-						been a cornerstone of wellness in the vibrant community
-						of Pappanchatiram. Our guiding individual is Dr. Ekta
-						Bharti M.B.B.S., a dedicated general physician who has
-						been leading the way to better health for our community.
+						For nearly a decade, Sundar Clinic has been a trusted
+						name in healthcare, committed to enhancing the
+						well-being of Pappanchatiram&apos;s residents. Our
+						journey began in 2013, and since then, we&apos;ve
+						evolved into a healthcare hub that&apos;s not just about
+						treating illnesses but also creating memorable
+						experiences for our patients.
+					</p>
+				</div>
+				<div className='bg-white p-4 rounded-lg shadow-lg max-w-4xl flex flex-col gap-4 mt-6 mx-auto'>
+					<h2 className='font-heading font-medium text-2xl md:text-4xl max-w-3xl'>
+						Our Approach
+					</h2>
+					<p className='text-lg md:text-xl text-justify max-w-5xl text-slate-500'>
+						Our approach is simple but profound: we provide
+						accessible and high-quality healthcare with a personal
+						touch. Situated in the heart of a developing area,
+						we&apos;ve been a beacon of hope for individuals seeking
+						answers to their health-related concerns. At Sundar
+						Clinic, we believe in the power of delegation, ensuring
+						that every patient receives the care they deserve,
+						precisely when they need it.
 					</p>
 				</div>
 			</div>
