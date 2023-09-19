c79e54bf88f2c071f90865d2ccad3b9138284511
kunalkeshan <kunalkeshan12@gmail.com>
2023-09-19 18:23:55 +0530

    chore: reuse home contact parts

--- app/(main)/contact/page.tsx
+++ app/(main)/contact/page.tsx
@@ -1,3 +1,19 @@
-export default function Contact() {
-	return <div>Contact</div>;
+/**
+ * Contact Page
+ */
+
+// Dependencies
+import { faqsQuery } from '@/sanity/lib/queries';
+import { sanityFetch } from '@/sanity/lib/sanityFetch';
+import Contact from '@/components/home/Contact';
+import FAQ from '@/components/home/FAQ';
+
+export default async function ContactPage() {
+	const faqs = await sanityFetch<FAQData>({ query: faqsQuery });
+	return (
+		<main>
+			<Contact />
+			<FAQ faqs={faqs} />
+		</main>
+	);
 }
