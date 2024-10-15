7631b16e6e7c9e89f86b935f043749906d8bbda5
Kunal Keshan <kunalkeshan12@gmail.com>
2024-10-15 15:13:25 +0530

    refactor: drop jest and use cypress

--- jest.setup.ts
+++
@@ -1,7 +0,0 @@
-import '@testing-library/jest-dom';
-import dotenv from 'dotenv';
-
-dotenv.config({ path: '.env.local' });
-
-global.TextEncoder = require('util').TextEncoder;
-global.TextDecoder = require('util').TextDecoder;
