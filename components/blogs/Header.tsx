679f059c6f6caf5ab0570c0555b94cd33d08edce
Kunal Keshan <kunalkeshan12@gmail.com>
2025-04-12 17:58:38 +0530

    refactor: update blog header layout from single row to text and image rows

--- components/blogs/Header.tsx
+++ components/blogs/Header.tsx
@@ -11,8 +11,16 @@ interface Props extends React.ComponentProps<"header"> {
 const BlogHeader: React.FC<Props> = ({ post, locale }) => {
   return (
     <header className="max-w-3xl mx-auto w-full p-4 mt-8">
-      <section className="flex flex-col lg:flex-row lg:gap-6 gap-4">
-        <div className="overflow-hidden rounded-lg lg:w-2/3">
+      <section className="flex flex-col gap-4">
+        <div className="flex flex-col justify-center gap-4">
+          <h1 className="font-heading font-medium text-2xl lg:text-4xl lg:text-left text-center">
+            {post?.title?.[locale]}
+          </h1>
+          <p className="text-lg lg:text-xl text-justify text-slate-500">
+            {post?.description?.[locale]}
+          </p>
+        </div>
+        <div className="overflow-hidden rounded-lg w-full">
           <Image
             src={
               (post.thumbnail &&
@@ -24,14 +32,6 @@ const BlogHeader: React.FC<Props> = ({ post, locale }) => {
             height={500}
             className="w-full h-auto aspect-video object-cover object-center"
           />
-        </div>
-        <div className="lg:w-1/3 flex flex-col justify-center gap-4">
-          <h1 className="font-heading font-medium text-2xl lg:text-4xl lg:text-left text-center">
-            {post?.title?.[locale]}
-          </h1>
-          <p className="text-lg lg:text-xl text-justify text-slate-500">
-            {post?.description?.[locale]}
-          </p>
         </div>
       </section>
     </header>
