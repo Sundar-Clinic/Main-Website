88b7ca7989f0d4a2a5fd57cdc1e789231e6c0d00
Sundar Clinic <105741467+sundarclinic@users.noreply.github.com>
2025-04-12 20:53:30 +0530

    Added Time based revalidation on slug pages (blogs & lab tests) (#134)
    * chore: added time based revalidation on blogs
    
    * chore: added time based revalidation on lab tests
    
    ---------
    
    Co-authored-by: Kunal Keshan <kunalkeshan12@gmail.com>

--- app/[locale]/blogs/[slug]/page.tsx
+++ app/[locale]/blogs/[slug]/page.tsx
@@ -1,46 +1,48 @@
-import React from 'react';
-import { postQuery } from '@/sanity/lib/queries';
-import { PostQueryResult } from '@/@types/cms';
-import { sanityFetch } from '@/sanity/lib/sanityFetch';
-import { notFound } from 'next/navigation';
-import BlogHeader from '@/components/blogs/Header';
-import BlogAuthor from '@/components/blogs/Author';
-import BlogCategories from '@/components/blogs/Categories';
-import BlogContent from '@/components/blogs/Content';
-import { unstable_setRequestLocale } from 'next-intl/server';
-import CTA from '@/components/cta/CTA';
-import SocialShare from '@/components/blogs/SocialShare';
+import React from "react";
+import { postQuery } from "@/sanity/lib/queries";
+import { PostQueryResult } from "@/@types/cms";
+import { sanityFetch } from "@/sanity/lib/sanityFetch";
+import { notFound } from "next/navigation";
+import BlogHeader from "@/components/blogs/Header";
+import BlogAuthor from "@/components/blogs/Author";
+import BlogCategories from "@/components/blogs/Categories";
+import BlogContent from "@/components/blogs/Content";
+import { unstable_setRequestLocale } from "next-intl/server";
+import CTA from "@/components/cta/CTA";
+import SocialShare from "@/components/blogs/SocialShare";
+
+export const revalidate = 3600;
 
 type IndividualBlogLayoutProps = {
-	params: {
-		locale: LocaleLanguages;
-		slug: string;
-	};
+  params: {
+    locale: LocaleLanguages;
+    slug: string;
+  };
 };
 
 const IndividualBlogPage: React.FC<IndividualBlogLayoutProps> = async ({
-	params: { locale, slug },
+  params: { locale, slug },
 }) => {
-	unstable_setRequestLocale(locale);
-	const post = await sanityFetch<PostQueryResult>({
-		query: postQuery,
-		params: { slug },
-	});
-	if (!post) {
-		return notFound();
-	}
-	return (
-		<article className='w-full'>
-			<BlogHeader post={post} locale={locale} />
-			<BlogContent post={post} locale={locale} />
-			{post.categories && post.categories.length > 0 && (
-				<BlogCategories post={post} locale={locale} />
-			)}
-			{post.author && <BlogAuthor post={post} locale={locale} />}
-			<SocialShare post={post} locale={locale} />
-			<CTA cta={post.cta} />
-		</article>
-	);
+  unstable_setRequestLocale(locale);
+  const post = await sanityFetch<PostQueryResult>({
+    query: postQuery,
+    params: { slug },
+  });
+  if (!post) {
+    return notFound();
+  }
+  return (
+    <article className="w-full">
+      <BlogHeader post={post} locale={locale} />
+      <BlogContent post={post} locale={locale} />
+      {post.categories && post.categories.length > 0 && (
+        <BlogCategories post={post} locale={locale} />
+      )}
+      {post.author && <BlogAuthor post={post} locale={locale} />}
+      <SocialShare post={post} locale={locale} />
+      <CTA cta={post.cta} />
+    </article>
+  );
 };
 
 export default IndividualBlogPage;
--- app/[locale]/lab/tests/[slug]/page.tsx
+++ app/[locale]/lab/tests/[slug]/page.tsx
@@ -1,40 +1,42 @@
-import React from 'react';
-import { sanityFetch } from '@/sanity/lib/sanityFetch';
-import { notFound } from 'next/navigation';
-import { unstable_setRequestLocale } from 'next-intl/server';
-import { LabTestQueryResult } from '@/@types/cms';
-import { labTestQuery } from '@/sanity/lib/queries';
-import LabTestHeader from '@/components/lab/Header';
-import TestInformation from '@/components/lab/TestInformation';
-import TestBody from '@/components/lab/TestBody';
+import React from "react";
+import { sanityFetch } from "@/sanity/lib/sanityFetch";
+import { notFound } from "next/navigation";
+import { unstable_setRequestLocale } from "next-intl/server";
+import { LabTestQueryResult } from "@/@types/cms";
+import { labTestQuery } from "@/sanity/lib/queries";
+import LabTestHeader from "@/components/lab/Header";
+import TestInformation from "@/components/lab/TestInformation";
+import TestBody from "@/components/lab/TestBody";
+
+export const revalidate = 3600;
 
 type IndividualLabTestLayoutProps = {
-	params: {
-		locale: LocaleLanguages;
-		slug: string;
-	};
+  params: {
+    locale: LocaleLanguages;
+    slug: string;
+  };
 };
 
 const IndividualBlogPage: React.FC<IndividualLabTestLayoutProps> = async ({
-	params: { locale, slug },
+  params: { locale, slug },
 }) => {
-	unstable_setRequestLocale(locale);
-	const test = await sanityFetch<LabTestQueryResult>({
-		query: labTestQuery,
-		params: { slug },
-	});
-	if (!test) {
-		return notFound();
-	}
-	return (
-		<main className='w-full'>
-			<LabTestHeader test={test} locale={locale} />
-			<section className='container grid gap-12 lg:grid-cols-3'>
-				<TestBody test={test} locale={locale} />
-				<TestInformation test={test} locale={locale} />
-			</section>
-		</main>
-	);
+  unstable_setRequestLocale(locale);
+  const test = await sanityFetch<LabTestQueryResult>({
+    query: labTestQuery,
+    params: { slug },
+  });
+  if (!test) {
+    return notFound();
+  }
+  return (
+    <main className="w-full">
+      <LabTestHeader test={test} locale={locale} />
+      <section className="container grid gap-12 lg:grid-cols-3">
+        <TestBody test={test} locale={locale} />
+        <TestInformation test={test} locale={locale} />
+      </section>
+    </main>
+  );
 };
 
 export default IndividualBlogPage;
