7dbf3690058a2f91b6d8de9dc83599f7ae737c5b
kunalkeshan <kunalkeshan12@gmail.com>
2025-06-18 01:20:32 +0530

    feat: enhance TestimonialCard component with Google Reviews link and improve layout

--- components/home/Testimonials.tsx
+++ components/home/Testimonials.tsx
@@ -3,40 +3,43 @@
  */
 
 // Dependencies
-import React from 'react';
-import TestimonialCard from '@/components/cards/TestimonialCard';
-import { useTranslations } from 'next-intl';
-import { TestimonialsQueryResult } from '@/@types/cms';
+import React from "react";
+import TestimonialCard, {
+  TestimonialCardLinkToGoogleReviews,
+} from "@/components/cards/TestimonialCard";
+import { useTranslations } from "next-intl";
+import { TestimonialsQueryResult } from "@/@types/cms";
 
-type TestimonailsProps = React.ComponentProps<'section'> & {
-	testimonials: TestimonialsQueryResult;
-	locale: LocaleLanguages;
+type TestimonailsProps = React.ComponentProps<"section"> & {
+  testimonials: TestimonialsQueryResult;
+  locale: LocaleLanguages;
 };
 
 const Testimonials: React.FC<TestimonailsProps> = ({
-	testimonials,
-	locale,
+  testimonials,
+  locale,
 }) => {
-	const t = useTranslations('pages.home.testimonials');
+  const t = useTranslations("pages.home.testimonials");
 
-	return (
-		<section className='container'>
-			<h3 className='text-2xl font-heading text-center font-medium'>
-				{t('heading')}
-			</h3>
-			<hr className='border-b-4 max-w-[10rem] border-b-primary-clinic rounded-lg mx-auto mt-2' />
-			<ul className='grid grid-cols-1 md:grid-cols-3 gap-8 mt-8 place-items-center'>
-				{testimonials &&
-					testimonials?.map((testimonial) => (
-						<TestimonialCard
-							key={testimonial._id}
-							locale={locale}
-							{...testimonial}
-						/>
-					))}
-			</ul>
-		</section>
-	);
+  return (
+    <section className="container">
+      <h3 className="text-2xl font-heading text-center font-medium">
+        {t("heading")}
+      </h3>
+      <hr className="border-b-4 max-w-[10rem] border-b-primary-clinic rounded-lg mx-auto mt-2" />
+      <ul className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-8 place-items-center">
+        {testimonials &&
+          testimonials?.map((testimonial) => (
+            <TestimonialCard
+              key={testimonial._id}
+              locale={locale}
+              {...testimonial}
+            />
+          ))}
+        <TestimonialCardLinkToGoogleReviews locale={locale} />
+      </ul>
+    </section>
+  );
 };
 
 export default Testimonials;
